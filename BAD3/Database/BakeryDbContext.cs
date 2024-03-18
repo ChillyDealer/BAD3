21d66478ad4c39d519803f0684256a0df0c3728a
au725202 <49028947+Oppai-Loli@users.noreply.github.com>
2024-03-18 15:30:55 +0100

    database done

--- BAD3/Database/BakeryDbContext.cs
+++ BAD3/Database/BakeryDbContext.cs
@@ -5,10 +5,37 @@ namespace Bad3.Database;
 
 public class BakeryDbContext : DbContext
 {
-    public BakeryDbContext(DbContextOptions<BakeryDbContext> options)
-        : base(options)
-    {
-    }
-    
-    public DbSet<Order> Orders => Set<Order>();
+	public BakeryDbContext(DbContextOptions<BakeryDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Customer> Customers => Set<Customer>();
+	public DbSet<Order> Orders => Set<Order>();
+	public DbSet<Goods> Goods => Set<Goods>();
+	public DbSet<Batch> Batches => Set<Batch>();
+	public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+	public DbSet<Stock> Stocks => Set<Stock>();
+	public DbSet<Delivery> Deliveries => Set<Delivery>();
+	public DbSet<Driver> Drivers => Set<Driver>();
+	public DbSet<GoodsOrder> GoodsOrders => Set<GoodsOrder>();
+	public DbSet<BatchGoods> BatchGoods => Set<BatchGoods>();
+	public DbSet<IngredientBatch> IngredientBatches => Set<IngredientBatch>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		// Goods + Order
+		modelBuilder.Entity<GoodsOrder>()
+			.HasKey(go => new { go.GoodsId, go.OrderId });
+
+		// Batch + Goods
+		modelBuilder.Entity<BatchGoods>()
+			.HasKey(bg => new { bg.BatchId, bg.GoodsId });
+
+		// Ingredient + Batch
+		modelBuilder.Entity<IngredientBatch>()
+			.HasKey(ib => new { ib.IngredientId, ib.BatchId });
+	}
 }
--- BAD3/Model/BatchGoods.cs
+++ BAD3/Model/BatchGoods.cs
@@ -1,6 +1,15 @@
-﻿namespace Bad3.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bad3.Model
 {
 	public class BatchGoods
 	{
+		[Key, ForeignKey("Batch")]
+		public int BatchId { get; set; }
+		public virtual Batch Batch { get; set; }
+		[Key, ForeignKey("Goods")]
+		public int GoodsId { get; set; }
+		public virtual Goods Goods { get; set; }
 	}
 }
