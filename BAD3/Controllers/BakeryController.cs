88bfde8cef9e50ef08637601a62a3be1da2ac8a8
OliverEC <oliverec04@gmail.com>
2024-03-18 12:35:10 +0100

    hello api

---
+++ BAD3/Controllers/BakeryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bad3.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class BakeryController : ControllerBase
+{
+    //[HttpGet(Name = "GetHello")]
+    
+}
