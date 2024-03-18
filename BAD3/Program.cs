fd509d1b73293db600398b893edcd6edd166de30
OliverEC <oliverec04@gmail.com>
2024-03-18 11:19:21 +0100

    Update Program.cs

--- BAD3/Program.cs
+++ BAD3/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace BAD3;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
